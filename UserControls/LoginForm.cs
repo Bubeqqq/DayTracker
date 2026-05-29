db46b803c7ea1dea6d48983cc5a3258ca5d7cb93
SpaceBoi-404 <pawelsoltysik35@gmail.com>
2026-05-29 21:28:22 +0200

    Zaimplementowano interfejs dla formularza logowania

--- UserControls/LoginForm.cs
+++ UserControls/LoginForm.cs
@@ -1,20 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
-namespace DayTracker.UserControls
+﻿namespace DayTracker.UserControls
 {
-    public partial class LoginForm : UserControl
+    public partial class LoginForm : UserControl, ILoginView
     {
+        public string Email => textBoxEmail.Text;
+
+        public string Password => textBoxPass.Text;
+
+        public bool IsPasswordHidden
+        {
+            get => textBoxPass.UseSystemPasswordChar;
+            set => textBoxPass.UseSystemPasswordChar = value;
+        }
+
+        public event Action? BtnLoginClicked;
+        public event Action? BtnRegisterClicked;
         public LoginForm()
         {
             InitializeComponent();
+            btnLogIn.Click += (s, e) => BtnLoginClicked?.Invoke();
+            btnRegister.Click += (s, e) => BtnRegisterClicked?.Invoke();
         }
     }
 }
