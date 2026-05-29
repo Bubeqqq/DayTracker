d4a94f06ff3a8f5602df0fa3834972926a208f5a
SpaceBoi-404 <pawelsoltysik35@gmail.com>
2026-05-29 21:21:20 +0200

    Utworzono podstawowe interfejsy dla formularzy do logowania i rejestracji

---
+++ UserControls/ILoginView.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DayTracker.UserControls
+{
+    internal interface ILoginView
+    {
+        string Email { get; }
+        string Password { get; }
+        bool IsPasswordHidden { get; set; }
+
+        event Action BtnLoginClicked;
+        event Action BtnRegisterClicked;
+    }
+}
---
+++ UserControls/IRegisterView.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DayTracker.UserControls
+{
+    internal interface IRegisterView
+    {
+        string Name { get; }
+        string LastName { get; }
+        string Email { get; }
+        string Password { get; }
+        string ConfirmPassword { get; }
+        bool IsPasswordHidden { get; set; }
+        bool IsConfirmPasswordHidden { get; set; }
+
+        event Action BtnRegisterClicked;
+        event Action BtnLoginClicked;
+    }
+}
