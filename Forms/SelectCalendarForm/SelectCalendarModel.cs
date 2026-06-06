5bed181b7dc535944ad0041e6ede7ff47f2a78f5
SpaceBoi-404 <pawelsoltysik35@gmail.com>
2026-06-06 13:03:00 +0200

    Prawie utworzono metodę do przygotowania współdzielonych kalendarzy do wyświetlenia w comboBoxie

--- Forms/SelectCalendarForm/SelectCalendarModel.cs
+++ Forms/SelectCalendarForm/SelectCalendarModel.cs
@@ -22,5 +22,25 @@ namespace DayTracker.Forms.SelectCalendarForm
 
             return OperationResult<string>.Failure("Current user is not set correctly!");
         }
+
+        public OperationResult<List<(int, string)>> GetUserSharedCalendars()
+        {
+            // work in progress
+            if (_loadedDataService.GetCurrentUser() is User currentUser)
+            {
+                var sharedCalendarIds = _loadedDataService.GetPermissions()
+                    .Where(p => p.UserId == currentUser.Id && p.CalendarId != currentUser.CalendarId)
+                    .Select(p => p.CalendarId).ToList();
+
+                if (sharedCalendarIds.Count == 0)
+                    return OperationResult<List<(int, string)>>.Failure("No shared calendars found");
+
+                //var sharedCalendars = _loadedDataService.GetPermissions()
+                //    .Where(p => sharedCalendarIds.Contains(p.CalendarId) && p.PermissionName == PermissionType.Admin)
+                //    .
+            }
+
+            return OperationResult<List<(int, string)>>.Failure("Current user is not set correctly!");
+        }
     }
 }
